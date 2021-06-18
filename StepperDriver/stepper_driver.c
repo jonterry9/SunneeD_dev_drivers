8d5c9c67c14844d6b33e6e975333c7bf5617346d
Jon Terry <jterry82@gwu.edu>
2021-06-18 14:22:39 -0400

    update stepper driver to use stdin/stdout

--- StepperDriver/stepper_driver.c
+++ StepperDriver/stepper_driver.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <signal.h>
+#include <errno.h>
+#include <string.h>
 #include "lib_ULN2003_stepper.h"
 
 int orientation_file, cur_orientation;
@@ -21,12 +23,11 @@ void sig_handler(int signo)
     exit(0);
 }
 
-int main(void)
-{
+int main(void) {
     unsigned char byte_1;
-    int fifo_fd, position, last_read, dif, dif2, ret, byte_2;
-    const char *path = "/dev/stepper";
-    static char *orientation_path = "orientation_rec.bin";
+    int fifo_fd, position, last_read, dif, dif2, ret, byte_2, sunneed_pipe_handler_pid;
+    const char *path = "/tmp/stepper";
+    static char *orientation_path = "/tmp/orientation_rec.bin";
     last_read = ret = -1;
     cur_orientation = 0;
 
@@ -36,8 +37,8 @@ int main(void)
     }
 
     if ((orientation_file = open(orientation_path,O_RDWR)) == -1) {
-        if ((orientation_file = open(orientation_path, O_CREAT | O_RDWR, S_IRWXU | S_IWOTH)) == -1) {
-            fprintf(stderr, "ERR: stepper lib can't create file\n");
+        if ((orientation_file = open(orientation_path, O_CREAT | O_RDWR)) == -1) {
+            fprintf(stderr, "ERR: stepper lib can't create file -- %s\n",strerror(errno));
             exit(1);
         }
     } else if (read(orientation_file, &cur_orientation, 2) == 0) {
@@ -45,24 +46,18 @@ int main(void)
         close(orientation_file);
         exit(1);
     }
-    if ((fifo_fd = open(path, O_RDONLY))  == -1) { /* open pipe for reading */
-        if (mkfifo(path, S_IRWXU | S_IWOTH) == -1) { /* if pipe doesn't exist, create named pipe */
-            fprintf(stderr, "ERR: stepper driver could not create pipe\n");
-            exit(1);
-        }
-        chmod(path, S_IWOTH);
-        if ((fifo_fd = open(path, O_RDONLY))  == -1) {
-            fprintf(stderr, "ERR: stepper driver could not open pipe");
-            exit(1);
-        }
-    }   
     while (1) {
-        if (read(fifo_fd, &byte_1, 1) != 0) {
+        if (read(0, &byte_1, 1) != 0) {
             if (byte_1 != '+' && byte_1 != '-') {
-                if (read(fifo_fd, &byte_2, sizeof(byte_2))) != 0) {
-                    position = (byte_2 << 8) | byte_1;
-                    if (position >= 0 && position <= 360 && position != last_read) { /* check for position validity & pointless calls (dont move motor)*/
-
+		if (byte_1 == 'o') {
+   		    if (write(1, &cur_orientation, sizeof(cur_orientation)) <= 0) {
+		        fprintf(stderr, "ERR: write to sunneed data pipe failed\n");
+		        exit(1);
+		    }
+		    continue;
+		} else if (read(0, &byte_2, 3) != 0) {
+    		        position = (byte_2 << 8) | byte_1;
+		    if (position >= 0 && position <= 360 && position != last_read) { /* check for position validity & pointless calls (dont move motor)*/
                         dif = cur_orientation - position;
                         dif2 = 360 - abs(dif);
                         cur_orientation = position;
@@ -82,8 +77,8 @@ int main(void)
                         last_read = position;
                     }
                 }
-            } else {
-                read(fifo_fd, &position, sizeof(int));
+            } else if (byte_1 != 'o'){
+                read(0, &position, sizeof(int));
                 if (position >= 0 && position <= 360) {
                     if (byte_1 == '+') {
                         rot_n_deg(position, 1);
@@ -92,7 +87,7 @@ int main(void)
                     }
                 }
             }
-        }
+	}  
     }
     exit(1); /* should never be reached */
 }
