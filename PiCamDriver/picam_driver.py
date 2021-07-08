71bb9ed034a7b8f408012f0a90bca13df1aca29b
Jon Terry <jterry82@gwu.edu>
2021-07-08 16:24:50 -0400

    have camera driver read from stdin rather than fd from open() call -- Sunneed sets stdin before driver executes

--- PiCamDriver/picam_driver.py
+++ PiCamDriver/picam_driver.py
@@ -1,40 +1,18 @@
-import os, sys
-import signal
+import sys
 from picamera import *
-
-#############################
-cam = PiCamera()
-#############################
-path = "/tmp/cam_driver"
-try:
-    fifo = open(path, 'r')
-except FileNotFoundError as e:
-    try:
-        os.mkfifo(path)
-    except OSError as e:
-        print("Failed to create FIFO: %s" %e)
-        exit(1)
-    else:
-        fifo = open(path, 'r')
-#############################
-
-def handle_exit(_signo, _stack_frame):
-    print("Camera driver exiting")
-    cam.close()
-    fifo.close()
-    sys.exit(0)
 
 def read_pipe():
     while True:
-        line = fifo.readline()
+        line = sys.stdin.readline()
         if (line is not None and line is not ""):
+            cam = PiCamera()
             try:
                 to_exec = "cam."
                 to_exec += line
                 exec(to_exec)
             except Exception as e:
                 print("Request to camera failed with: %s" %e)
+            cam.close()
 
-signal.signal(signal.SIGINT, handle_exit)
-signal.signal(signal.SIGTERM, handle_exit)
+
 read_pipe()
